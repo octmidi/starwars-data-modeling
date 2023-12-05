0d8cdd8ad70816fedc00076661cfb2ba5a5653e4
octmidi <128103131+octmidi@users.noreply.github.com>
2023-12-05 10:15:49 -0300

    fisrt commit model data base star wars

--- src/models.py
+++ src/models.py
@@ -7,23 +7,42 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
+class Usuario(Base):
+    __tablename__ = 'usuario'
+    # Here we define columns for the table usuario
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
+    password = Column(String(250),nullable=False )
     name = Column(String(250), nullable=False)
+    mail = Column(String(250), nullable= False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
+class Planeta(Base):
+    __tablename__ = 'planeta'
+    # Here we define columns for the table planeta.
     # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key = True)
+    name = Column(String(250), nullable = False)   
+
+class Personaje(Base):
+    __tablename__='personaje'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250),nullable = False)
+    planeta_id= Column(Integer, ForeignKey('planeta.id'))
+
+class Favorito(Base):
+    __tablename__='favorito'
+    id = Column(Integer, primary_key=True)
+    usario_id = Column(Integer, ForeignKey('usuario.id'))
+    planeta_id= Column(Integer, ForeignKey('planeta.id'))
+    personaje_id = Column(Integer),ForeignKey('personaje.id') 
+
+
+class Post(Base):
+    __tablename__='post'
+    id = Column(Integer, primary_key=True)
+    usario_id = Column(Integer, ForeignKey('usuario.id'))
+    contenido = Column(String(250))
+
 
     def to_dict(self):
         return {}
